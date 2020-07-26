e3b1474a3c374e33a8f2b7a3be6716e5faf2ad71
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-07-26 16:33:15 +0430

    linkedlist.h file added

---
+++ linkedList.h
@@ -0,0 +1,25 @@
+/*
+ *              In The Name of God 
+ * 
+ * ==========================================
+ * [] File Name:  linkedList.h
+ * 
+ * [] Creation Date:  26-00-2020
+ * 
+ * [] Created By:  Mohammad Mahdi Malmasi (m.mahdi.m79@gmail.com / https://github.com/MmahdiM79)
+ * ==========================================
+ */
+/*
+ *  Copyright ©️  2020  Mohammad Mahdi Malmasi.
+ */
+
+
+#ifndef __LINKEDLIST_H__
+#define __LINKEDLIST_H__
+
+
+
+
+
+
+#endif
