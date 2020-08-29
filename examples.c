9d1658b285d1f5a38b6b7312b374c0365de1d516
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-08-29 20:15:03 +0430

    int list head example added

--- examples.c
+++ examples.c
@@ -5,5 +5,20 @@
 
 int main(int n, char *args[])
 {
+    /*              =============  int node functions ============              */
+
+    // for each int list you need a list head
+    // so first of all we make a pointer of INT_LIST_HEAD
+    // for this, we use new_int_list_head() function
+    INT_LIST_HEAD *head = new_int_list_head();
+
+    // there is a easiest way to make a pointer of INT_LIST_HEAD
+    // for this we used a defined string
+    ilh h = new_int_list_head();
+
+
+
+
+
     return 0; 
 }
