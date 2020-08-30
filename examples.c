2941a272be2dd3ee58d06dccc78c100b96026b91
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-08-30 18:45:29 +0430

    create new int node part added

--- examples.c
+++ examples.c
@@ -7,6 +7,9 @@ int main(int n, char *args[])
 {
     /*              =============  int node functions ============              */
 
+
+    //      *  create a new int list head  *
+
     // for each int list you need a list head
     // so first of all we make a pointer of INT_LIST_HEAD
     // for this, we use new_int_list_head() function
@@ -17,6 +20,17 @@ int main(int n, char *args[])
     ilh h = new_int_list_head();
 
 
+
+    //      *  create a new int node  *
+
+    // now you need some int nodes for your list
+    // you need the pointer of int nodes to add them to the list
+    // for this we use new_int_node(value) function
+    INT_NODE *intNode = new_int_node(0);
+
+    // like INT_LIST_HEAD, 
+    // there is a easiest way to make a pointer of INT_NODE
+    iNode ind = new_int_node(1);
 
 
 
